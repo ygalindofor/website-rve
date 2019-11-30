cb19eb72f5d49dcfb9a1b4c4c99cd596cd1128f6
ygalindofor <57812527+ygalindofor@users.noreply.github.com>
2019-11-30 01:23:35 -0500

    Add files via upload

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "atrrlbdowohgjl"); // Usuario
+define('DB_PASSWORD', "8c304d72d03d205582f0b41f1ba428b2bf12940d6dc20ae68413e41fd13c7b33"); // Password
+define('DB_DATABASE', "d8eg65mufg9t31"); // Nombre de la base de datos
+define('DB_SERVER', "ec2-54-247-72-30.eu-west-1.compute.amazonaws.com"); //host server
+?>
