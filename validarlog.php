9435d087d34974f31777217ed0fac250ba93cb2e
ygalindofor <57812527+ygalindofor@users.noreply.github.com>
2019-11-30 01:41:17 -0500

    Update validarlog.php

--- validarlog.php
+++ validarlog.php
@@ -1,8 +1,8 @@
 <?php
-$server="localhost";
-$usuario="root";
-$contraseña="";
-$bd="proyecto";
+$usuario=define('DB_USER', "atrrlbdowohgjl"); // Usuario
+$contraseña=define('DB_PASSWORD', "8c304d72d03d205582f0b41f1ba428b2bf12940d6dc20ae68413e41fd13c7b33"); // Password
+$bd=define('DB_DATABASE', "d8eg65mufg9t31"); // Nombre de la base de datos
+$server=define('DB_SERVER', "ec2-54-247-72-30.eu-west-1.compute.amazonaws.com"); //host server
 
 
 $conexion=mysqli_connect($server,$usuario,$contraseña,$bd);
