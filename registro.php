70051ac20871facd372764be47beffe00c603fc0
ygalindofor <57812527+ygalindofor@users.noreply.github.com>
2019-11-30 01:34:20 -0500

    Update registro.php

--- registro.php
+++ registro.php
@@ -5,13 +5,12 @@
 </head>
 <body>
 <?php
-$server="localhost";
-$usuario="root";
-$contraseña="";
-$bd="proyecto";
+	
+$usuario=define('DB_USER', "atrrlbdowohgjl"); // Usuario
+$contraseña=define('DB_PASSWORD', "8c304d72d03d205582f0b41f1ba428b2bf12940d6dc20ae68413e41fd13c7b33"); // Password
+$bd=define('DB_DATABASE', "d8eg65mufg9t31"); // Nombre de la base de datos
+$server=define('DB_SERVER', "ec2-54-247-72-30.eu-west-1.compute.amazonaws.com"); //host server
 
-$conexion=mysqli_connect($server,$usuario,$contraseña,$bd)
-or die ("Error de la conexion");
 
 $cod_coor=$_POST['txtcodigo'];
 $nom_coor=$_POST['txtnombre'];
@@ -23,10 +22,7 @@ $pass_coor=$_POST['txtpass_coor'];
 $insertar="INSERT into coordinador (cod_coor,nom_coor,email_coor,cod_inst, pass_coor)
 					  values('$cod_coor','$nom_coor','$email_coor','$cod_inst', '$pass_coor')";
 
-$resultado=mysqli_query($conexion,$insertar)
-or die("error al Insertar los Registros".mysqli_error($conexion));
 
-mysqli_close($conexion);
 
 require("registrar.php");
 ?>
